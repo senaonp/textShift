6199014948404de64fd4175b66ed889bcfae3b40
Fran B <fran@fbacarisas.xyz>
2021-07-20 18:55:33 -0400

    import elemHelpers

---
+++ elemHelpers.js
@@ -0,0 +1,21 @@
+// shorthand selector by class or id
+var elemSelector = function(elemString) {
+	return {
+		".": document.getElementsByClassName(elemString.slice(1)),
+		"#": document.getElementById(elemString.slice(1)),
+	}[elemString[0]];
+};
+
+// pseudo-href for navbar elements
+var navi = function(path) {
+	location.href = path;
+};
+
+// close banner if open
+var toggleBanner = function() {
+	if (elemSelector('#banner').style.display == 'inline-block') {
+		elemSelector('#banner').style.display='none';
+	} else {
+		elemSelector('#banner').style.display='inline-block'
+	}
+};
