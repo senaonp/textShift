90970e3c03c8849d3550a87496ff97f9838868fb
senaonp <71605688+senaonp@users.noreply.github.com>
2021-08-05 19:04:27 -0400

    update website description

--- pageRender.js
+++ pageRender.js
@@ -28,32 +28,29 @@ elemSelector("#content").innerHTML = `
 			<small id="cipherL">cipher text length: 0</small>
 		</div>
         <textarea id="cipherTextInput" oninput="evalCipher(this)"></textarea>
-    </div>
-`;
+    </div>`;
 
 elemSelector("#notes").innerHTML = `
 	<div id="notesElem">
 		<p class="textareaName">notes</p>
 		<small>you can add any notes here; this field uses the browser's localStorage, so information is saved between browser sessions</small>
 		<textarea id="notesText" oninput="updateNotes(this)"></textarea>
-	</div>
-`;
+	</div>`;
 
 elemSelector("#textShiftDescription").innerHTML = `
-    <span id="version">textShift ver.1.1.23</span><br /><br />
+    <span id="version">textShift ver.1.2.0</span><br /><br />
     <br>any updates to the website will be added to the Github repository (<a target="_blank" href="https://github.com/senaonp/textShift">https://github.com/senaonp/textShift</a>).
-	<br><br>encrypting is applied when text is entered into the "text" field; decrypting is applied when text is entered into the "cipher text" field<br><hr><br>the current features of textShift are:<br>
+	<br><br>encoding is applied when text is entered into the "text" field; decoding is applied when text is entered into the "cipher text" field<br><hr><br>the current features of textShift are:<br>
     <div id="features">
-	<span class="feature">cipher options for encrypting / decrypting</span>
-    <span class="feature_detail">shifting each character's Unicode value by a set integer</span>
-    <span class="feature_detail">shifting every nth character's Unicode value by a set integer</span>
-	<span class="feature_detail">shifting each character's Unicode value by a set multiple</span>
-    <span class="feature_detail">reversing text cipher</span>
-    <span class="feature_detail">options to specify offset for each nth character</span>
-	<span class="feature_detail">options to specify by partition(s)</span>
-	<span class="feature">responsive output when entering either text input or cipherText input</span>
+	<span class="feature">cipher options for encoding and decoding</span>
+	<span class="feature_detail">cipher text functions such as shift, swap, insert, move, reverse, multiply, sequence</span>
+	<span class="feature_detail">cipher text selection such as by partition, nth chars, index range, step sequence, character set, offset</span>
+    <span class="feature">responsive output when entering either text input or cipherText input</span>
     <span class="feature">usage information on input fields</span>
+	<span class="feature">error handling for specific Unicode characters</span>
+	<span class="feature">a notes section</span>
     <span class="feature">responsive styling for mobile devices</span>
+	
     </div>
 	<br>
 	my other programming projects can also be viewed here: (<a target="_blank" href="https://fbacarisas.xyz/video/#programming_playlist">fbacarisas.xyz/video/#programming_playlist</a>)
